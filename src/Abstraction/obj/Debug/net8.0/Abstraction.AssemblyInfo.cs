0bd7addbd3569d5a921ce9c80eb232b09992f99c
Thanh LeMinh(thanhlm1) <minhthanh1200@hotmail.com>
2023-12-01 17:42:04 +0700

    add domain

--- src/Abstraction/obj/Debug/net8.0/Abstraction.AssemblyInfo.cs
+++ src/Abstraction/obj/Debug/net8.0/Abstraction.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Abstraction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05a0cbeb86e735b0e50c6ecb753acb7030a4a0f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Abstraction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Abstraction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
